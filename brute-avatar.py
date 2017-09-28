330dfd0eb8da28195cacb319384d7a4c22138844
Marcus Schimizzi <marcjschimizzi@gmail.com>
2017-09-28 01:36:11 -0400

    read stuff

--- brute-avatar.py
+++ brute-avatar.py
@@ -1,8 +1,14 @@
 import time
 
-# Declare functions
-def read_wff ():
 
+
+# Declare functions
+def read_wff (file, wff):
+    f = open(file, 'w')
+    line = f.readline()
+    line = line.split(" ")
+    wff['problem'] = line[1];
+    return wff
 
 def generate_assignment ():
 
